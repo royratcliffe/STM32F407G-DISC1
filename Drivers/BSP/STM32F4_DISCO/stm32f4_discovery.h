73df2d4cae1e1cf1ffb82bf2268bb55cb57f66b6
Roy Ratcliffe <roy@ratcliffe.me>
2025-06-18 13:52:58 +0100

    chore: clean up whitespace and formatting in stm32f4_discovery header and source files

--- Drivers/BSP/STM32F4_DISCO/stm32f4_discovery.h
+++ Drivers/BSP/STM32F4_DISCO/stm32f4_discovery.h
@@ -1,8 +1,8 @@
-/** 
+/**
   ******************************************************************************
   * @file    stm32f4_discovery.h
   * @author  MCD Application Team
-  * @brief   This file contains definitions for STM32F4-Discovery Kit's Leds and 
+  * @brief   This file contains definitions for STM32F4-Discovery Kit's Leds and
   *          push-button hardware resources.
   ******************************************************************************
   * @attention
@@ -16,7 +16,7 @@
   *
   ******************************************************************************
   */
-  
+
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32F4_DISCOVERY_H
 #define __STM32F4_DISCOVERY_H
@@ -24,26 +24,26 @@
 #ifdef __cplusplus
  extern "C" {
 #endif
-                                              
+
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
-   
+
 /** @addtogroup BSP
   * @{
-  */ 
+  */
 
 /** @addtogroup STM32F4_DISCOVERY
   * @{
-  */ 
-      
+  */
+
 /** @addtogroup STM32F4_DISCOVERY_LOW_LEVEL
   * @{
-  */ 
+  */
 
 /** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_Exported_Types STM32F4 DISCOVERY LOW LEVEL_Exported_Types
   * @{
   */
-typedef enum 
+typedef enum
 {
   LED4 = 0,
   LED3 = 1,
@@ -51,27 +51,27 @@ typedef enum
   LED6 = 3
 } Led_TypeDef;
 
-typedef enum 
-{  
+typedef enum
+{
   BUTTON_KEY = 0,
 } Button_TypeDef;
 
-typedef enum 
-{  
+typedef enum
+{
   BUTTON_MODE_GPIO = 0,
   BUTTON_MODE_EXTI = 1
-} ButtonMode_TypeDef;     
+} ButtonMode_TypeDef;
 /**
   * @}
-  */ 
+  */
 
 /** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_Exported_Constants STM32F4 DISCOVERY LOW LEVEL Exported Constants
   * @{
-  */ 
+  */
 
-/** 
-* @brief  Define for STM32F4_DISCOVERY board  
-*/ 
+/**
+* @brief  Define for STM32F4_DISCOVERY board
+*/
 #if !defined (USE_STM32F4_DISCO)
  #define USE_STM32F4_DISCO
 #endif
@@ -83,23 +83,23 @@ typedef enum
 
 #define LED4_PIN                         GPIO_PIN_12
 #define LED4_GPIO_PORT                   GPIOD
-#define LED4_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOD_CLK_ENABLE()  
-#define LED4_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOD_CLK_DISABLE()  
+#define LED4_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOD_CLK_ENABLE()
+#define LED4_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOD_CLK_DISABLE()
 
 #define LED3_PIN                         GPIO_PIN_13
 #define LED3_GPIO_PORT                   GPIOD
-#define LED3_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOD_CLK_ENABLE()  
-#define LED3_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOD_CLK_DISABLE()  
-  
+#define LED3_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOD_CLK_ENABLE()
+#define LED3_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOD_CLK_DISABLE()
+
 #define LED5_PIN                         GPIO_PIN_14
 #define LED5_GPIO_PORT                   GPIOD
-#define LED5_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOD_CLK_ENABLE()  
-#define LED5_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOD_CLK_DISABLE()  
+#define LED5_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOD_CLK_ENABLE()
+#define LED5_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOD_CLK_DISABLE()
 
 #define LED6_PIN                         GPIO_PIN_15
 #define LED6_GPIO_PORT                   GPIOD
-#define LED6_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOD_CLK_ENABLE()  
-#define LED6_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOD_CLK_DISABLE()  
+#define LED6_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOD_CLK_ENABLE()
+#define LED6_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOD_CLK_DISABLE()
 
 #define LEDx_GPIO_CLK_ENABLE(__INDEX__) do{if((__INDEX__) == 0) LED4_GPIO_CLK_ENABLE(); else \
                                            if((__INDEX__) == 1) LED3_GPIO_CLK_ENABLE(); else \
@@ -114,12 +114,12 @@ typedef enum
                                             }while(0)
 /**
   * @}
-  */ 
-  
+  */
+
 /** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_BUTTON STM32F4 DISCOVERY LOW LEVEL BUTTON
   * @{
-  */  
-#define BUTTONn                          1 
+  */
+#define BUTTONn                          1
 
 /**
  * @brief Wakeup push-button
@@ -128,7 +128,7 @@ typedef enum
 #define KEY_BUTTON_GPIO_PORT          GPIOA
 #define KEY_BUTTON_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOA_CLK_ENABLE()
 #define KEY_BUTTON_GPIO_CLK_DISABLE() __HAL_RCC_GPIOA_CLK_DISABLE()
-#define KEY_BUTTON_EXTI_IRQn          EXTI0_IRQn 
+#define KEY_BUTTON_EXTI_IRQn          EXTI0_IRQn
 
 #define BUTTONx_GPIO_CLK_ENABLE(__INDEX__)    do{if((__INDEX__) == 0) KEY_BUTTON_GPIO_CLK_ENABLE(); \
                                                 }while(0)
@@ -137,11 +137,11 @@ typedef enum
                                                  }while(0)
 /**
   * @}
-  */ 
-  
+  */
+
 /** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_BUS STM32F4 DISCOVERY LOW LEVEL BUS
   * @{
-  */  
+  */
 
 /*############################### SPI1 #######################################*/
 #define DISCOVERY_SPIx                              SPI1
@@ -158,7 +158,7 @@ typedef enum
    on accurate values, they just guarantee that the application will not remain
    stuck if the SPI communication is corrupted.
    You may modify these timeout values depending on CPU frequency and application
-   conditions (interrupts routines ...). */   
+   conditions (interrupts routines ...). */
 #define SPIx_TIMEOUT_MAX                            0x1000 /*<! The value of the maximal timeout for BUS waiting loops */
 
 
@@ -180,7 +180,7 @@ typedef enum
 #define DISCOVERY_I2Cx_FORCE_RESET()              __HAL_RCC_I2C1_FORCE_RESET()
 #define DISCOVERY_I2Cx_RELEASE_RESET()            __HAL_RCC_I2C1_RELEASE_RESET()
 
-/* I2C interrupt requests */                  
+/* I2C interrupt requests */
 #define DISCOVERY_I2Cx_EV_IRQn                    I2C1_EV_IRQn
 #define DISCOVERY_I2Cx_ER_IRQn                    I2C1_ER_IRQn
 
@@ -188,14 +188,14 @@ typedef enum
    on accurate values, they just guarantee that the application will not remain
    stuck if the SPI communication is corrupted.
    You may modify these timeout values depending on CPU frequency and application
-   conditions (interrupts routines ...). */   
+   conditions (interrupts routines ...). */
 #define I2Cx_TIMEOUT_MAX    0x1000 /*<! The value of the maximal timeout for BUS waiting loops */
 
 
 /*############################# ACCELEROMETER ################################*/
 /* Read/Write command */
-#define READWRITE_CMD                     ((uint8_t)0x80) 
-/* Multiple byte read/write command */ 
+#define READWRITE_CMD                     ((uint8_t)0x80)
+/* Multiple byte read/write command */
 #define MULTIPLEBYTE_CMD                  ((uint8_t)0x40)
 /* Dummy Byte Send by the SPI Master device in order to generate the Clock to the Slave device */
 #define DUMMY_BYTE                        ((uint8_t)0x00)
@@ -215,12 +215,12 @@ typedef enum
 #define ACCELERO_INT_GPIO_CLK_ENABLE()         __HAL_RCC_GPIOE_CLK_ENABLE()
 #define ACCELERO_INT_GPIO_CLK_DISABLE()        __HAL_RCC_GPIOE_CLK_DISABLE()
 #define ACCELERO_INT1_PIN                      GPIO_PIN_0                 /* PE.00 */
-#define ACCELERO_INT1_EXTI_IRQn                EXTI0_IRQn 
+#define ACCELERO_INT1_EXTI_IRQn                EXTI0_IRQn
 #define ACCELERO_INT2_PIN                      GPIO_PIN_1                 /* PE.01 */
-#define ACCELERO_INT2_EXTI_IRQn                EXTI1_IRQn 
+#define ACCELERO_INT2_EXTI_IRQn                EXTI1_IRQn
 /**
   * @}
-  */ 
+  */
 
 
 /*############################### AUDIO ######################################*/
@@ -228,21 +228,21 @@ typedef enum
   * @brief  AUDIO I2C Interface pins
   */
 #define AUDIO_I2C_ADDRESS                     0x94
-  
+
   /* Audio Reset Pin definition */
 #define AUDIO_RESET_GPIO_CLK_ENABLE()         __HAL_RCC_GPIOD_CLK_ENABLE()
 #define AUDIO_RESET_PIN                       GPIO_PIN_4
 #define AUDIO_RESET_GPIO                      GPIOD
 /**
   * @}
-  */  
+  */
 
 /** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_Exported_Macros STM32F4 DISCOVERY LOW LEVEL Exported Macros
   * @{
-  */  
+  */
 /**
   * @}
-  */ 
+  */
 
 /** @defgroup STM32F4_DISCOVERY_LOW_LEVEL_Exported_Functions STM32F4 DISCOVERY LOW LEVEL Exported Functions
   * @{
@@ -265,16 +265,16 @@ uint32_t BSP_PB_GetState(Button_TypeDef Button);
 
 /**
   * @}
-  */ 
-
-/**
-  * @}
-  */ 
+  */
 
 /**
   * @}
   */
-  
+
+/**
+  * @}
+  */
+
 #ifdef __cplusplus
 }
 #endif
