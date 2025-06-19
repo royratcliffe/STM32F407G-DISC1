fc7e90a88089a9faf963d86e97bbc400424fcab9
Roy Ratcliffe <roy@ratcliffe.me>
2025-06-19 15:05:48 +0100

    Add FreeRTOS+IO component with initial header files and utility functions
    - Created header files for FreeRTOS+IO including FreeRTOS_IO.h, FreeRTOS_i2c.h, FreeRTOS_ssp.h, FreeRTOS_uart.h.
    - Implemented character queue and circular buffer utilities in IOUtils_CharQueueTxAndRx.h and IOUtils_CircularBufferRx.h.
    - Added zero copy transmission utilities in IOUtils_ZeroCopyTx.h.
    - Included a demo link and readme file for FreeRTOS+IO documentation.
    - Ensured proper licensing information is included in all new files.

---
+++ Middlewares/Third_Party/FreeRTOS-Plus-IO/Device/STM32xx/STM32xx-base-board.h
@@ -0,0 +1,67 @@
+/*
+ * FreeRTOS+IO V1.0.1 (C) 2012 Real Time Engineers ltd.
+ *
+ * FreeRTOS+IO is an add-on component to FreeRTOS.  It is not, in itself, part
+ * of the FreeRTOS kernel.  FreeRTOS+IO is licensed separately from FreeRTOS,
+ * and uses a different license to FreeRTOS.  FreeRTOS+IO uses a dual license
+ * model, information on which is provided below:
+ *
+ * - Open source licensing -
+ * FreeRTOS+IO is a free download and may be used, modified and distributed
+ * without charge provided the user adheres to version two of the GNU General
+ * Public license (GPL) and does not remove the copyright notice or this text.
+ * The GPL V2 text is available on the gnu.org web site, and on the following
+ * URL: http://www.FreeRTOS.org/gpl-2.0.txt
+ *
+ * - Commercial licensing -
+ * Businesses and individuals who wish to incorporate FreeRTOS+IO into
+ * proprietary software for redistribution in any form must first obtain a low
+ * cost commercial license - and in-so-doing support the maintenance, support
+ * and further development of the FreeRTOS+IO product.  Commercial licenses can
+ * be obtained from http://shop.freertos.org and do not require any source files
+ * to be changed.
+ *
+ * FreeRTOS+IO is distributed in the hope that it will be useful.  You cannot
+ * use FreeRTOS+IO unless you agree that you use the software 'as is'.
+ * FreeRTOS+IO is provided WITHOUT ANY WARRANTY; without even the implied
+ * warranties of NON-INFRINGEMENT, MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. Real Time Engineers Ltd. disclaims all conditions and terms, be they
+ * implied, expressed, or statutory.
+ *
+ * 1 tab == 4 spaces!
+ *
+ * http://www.FreeRTOS.org
+ * http://www.FreeRTOS.org/FreeRTOS-Plus
+ *
+ */
+
+#ifndef STM32XX_BASE_BOARD_H
+#define STM32XX_BASE_BOARD_H
+
+/* Header files for all the driver libraries that can be used with this BSP. */
+
+/*******************************************************************************
+ * Definitions used by FreeRTOS+IO to determine the peripherals that are
+ * available on the board, and the functions used to interface with the target
+ * specific peripheral drivers.
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Definitions used by the UART-interrupt-driven-command-console.c example file.
+ *
+ * See http://www.freertos.org/FreeRTOS-Plus/FreeRTOS_Plus_IO/Board_Support_Packages.shtml
+ *
+ ******************************************************************************/
+#define boardAVAILABLE_DEVICES_LIST												\
+{																				\
+	{ ( const int8_t * const ) "/ACM0/", eUART_TYPE, ( void * ) NULL }		\
+}
+
+
+/*******************************************************************************
+ * Map the FreeRTOS+IO interface to the LPC17xx specific functions.
+ ******************************************************************************/
+portBASE_TYPE vFreeRTOS_stm32xx_PopulateFunctionPointers( const Peripheral_Types_t ePeripheralType, Peripheral_Control_t * const pxPeripheralControl );
+#define boardFreeRTOS_PopulateFunctionPointers vFreeRTOS_stm32xx_PopulateFunctionPointers
+
+#endif /* STM32XX_BASE_BOARD_H */
