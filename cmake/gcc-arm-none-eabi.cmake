1147d2652fd0ceac4bf91e6101c56a311c5e099e
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-19 13:38:28 +0000

    fix: update comment to specify QEMU System ARM emulator for cross-compiling tests

--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -44,7 +44,7 @@ set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--print-memory-usage")
 
 set(CMAKE_CXX_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lstdc++ -lsupc++ -Wl,--end-group")
 
-# Emulator for cross-compiling tests.
+# QEMU System ARM emulator configuration for cross-compiling tests.
 set(CMAKE_CROSSCOMPILING_EMULATOR qemu-system-arm
     -machine netduinoplus2
     -nographic
