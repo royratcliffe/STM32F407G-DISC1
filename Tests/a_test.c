194184f46a9b06b4f7589e35b4645a0c4284140d
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-20 12:28:13 +0000

    style: add blank lines for improved readability in a_test.c

--- Tests/a_test.c
+++ Tests/a_test.c
@@ -11,6 +11,7 @@ int main(void) {
   (void)printf("Hello, World from %s!!!\n", "a_test");
 
   RING_BUF_DEFINE(buf, sizeof(float[4U]));
+
   float number = 1.0F;
   ring_buf_size_t ack;
   while ((ack = ring_buf_put(&buf, &number, sizeof(number)))) {
@@ -18,14 +19,17 @@ int main(void) {
     assert(err == 0);
     number += 1.0F;
   }
+
   float sum = 0.0F;
   void *space;
   while ((ring_buf_get_claim(&buf, &space, sizeof(float))))
     sum += *(float *)space;
   assert(sum == 1.0F + 2.0F + 3.0F + 4.0F);
+
   assert(ring_buf_free_space(&buf) == 0U);
   ring_buf_get_ack(&buf, 0U);
   assert(ring_buf_used_space(&buf) == sizeof(float[4U]));
+
   while ((ack = ring_buf_get(&buf, &number, sizeof(number)))) {
     int err = ring_buf_get_ack(&buf, ack);
     assert(err == 0);
@@ -34,6 +38,7 @@ int main(void) {
     (void)fcvtfprintf(number, 3);
     (void)printf("\n");
   }
+
   assert(ring_buf_free_space(&buf) == sizeof(float[4U]));
   assert(ring_buf_used_space(&buf) == 0U);
 
