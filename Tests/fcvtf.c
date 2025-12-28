f85c4d4d5898039d39eb7c1831cc169b0e6a6dca
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-28 22:02:16 +0000

    feat: add cvtfbuf function to convert float to string with user-provided buffer

--- Tests/fcvtf.c
+++ Tests/fcvtf.c
@@ -1,6 +1,8 @@
 #include "fcvtf.h"
 
+#include <malloc.h>
 #include <stdio.h>
+#include <string.h>
 
 extern char *fcvtf(float d, int ndigit, int *decpt, int *sign);
 
@@ -20,4 +22,42 @@ const char *_fcvtf(float d, int ndigit) {
   const char *buf = fcvtf(d, ndigit, &decpt, &sign);
   (void)snprintf(str, sizeof(str), "%s%.*s.%s", sign ? "-" : "", decpt, buf, buf + decpt);
   return str;
+}
+
+char *cvtfbuf(float f, int ndigit, char *buf) {
+  /*
+   * Allocate buffer space from the heap if not provided by the user. As a
+   * reasonable estimate, ask for ndigit + 36 characters to accommodate the
+   * digits, decimal point, sign, and possible leading "0." and extra zeros.
+   * Remember to free the buffer after use if it was allocated here.
+   */
+  if (buf == NULL) {
+    if ((buf = (char *)realloc(buf, ndigit + 36)) == NULL)
+      return NULL;
+  }
+  int decpt, sign;
+  const char *cvt = fcvtf(f, ndigit, &decpt, &sign);
+  char *ptr = buf;
+  if (sign) {
+    *ptr++ = '-';
+  }
+  /*
+   * Construct the final part of the string. If the decimal point is before the
+   * first digit, insert a leading "0." and the appropriate number of zeros.
+   * Otherwise, copy the digits before the decimal point,
+   */
+  if (decpt <= 0) {
+    *ptr++ = '0';
+    *ptr++ = '.';
+    for (int i = 0; i < -decpt; i++) {
+      *ptr++ = '0';
+    }
+    (void)strcpy(ptr, cvt);
+  } else {
+    (void)strncpy(ptr, cvt, decpt);
+    ptr += decpt;
+    *ptr++ = '.';
+    (void)strcpy(ptr, cvt + decpt);
+  }
+  return buf;
 }
--- Tests/fcvtf.h
+++ Tests/fcvtf.h
@@ -30,3 +30,20 @@ int fcvtfprintf(float d, int ndigit);
  * call.
  */
 const char *_fcvtf(float d, int ndigit);
+
+/*!
+ * \brief Convert a float to string using \c fcvtf() into a user-provided buffer.
+ * \details Converts the float number \p f with \p ndigit digits after the
+ * decimal point to a string using the legacy function fcvtf(). The result is
+ * stored in the user-provided buffer \p buf. If \p buf is \c NULL, a new buffer
+ * is allocated using the standard library's \c realloc() function.
+ * \param f Float number to convert.
+ * \param ndigit Number of digits to convert after the decimal point.
+ * \param buf User-provided buffer to store the result. If \c NULL, a new buffer
+ * is allocated.
+ * \return Pointer to the converted string in the provided or newly-allocated
+ * buffer. Returns \c NULL if memory allocation fails.
+ * \note The caller is responsible for freeing the returned buffer if it was
+ * allocated by the function (i.e., when \p buf is \c NULL).
+ */
+char *cvtfbuf(float f, int ndigit, char *buf);
