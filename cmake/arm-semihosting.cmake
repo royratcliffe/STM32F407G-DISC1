fc31c03f54bb8eb3d47b4c51227f66b9affe4236
Roy Ratcliffe <roy@ratcliffe.me>
2025-12-30 17:17:02 +0000

    Add ARM semihosting support and improve CMake configuration (#3)
    * Add ARM semihosting support and refactor CMake configuration
    
    Refactors semihosting test setup into reusable CMake module
    
    Moves ARM semihosting test configuration into a dedicated CMake
    module to improve maintainability and encourage reuse. Updates
    the main build script to include the new module, reducing
    duplication and clarifying project structure.
    
    Facilitates easier extension of semihosting test definitions for
    future development.
    
    * Removes trailing slash from module path variable
    
    Eliminates the trailing slash when setting the CMake module path,
    reducing the risk of duplicate slashes in path concatenation and
    improving compatibility with certain CMake module search behaviours.
    
    * chore: add SPDX license identifiers to arm-semihosting.cmake
    
    - Added SPDX license identifier and copyright notice at the top of the file.
    
    * fix: correct command for adding test in add_arm_semihosting_test
    
    - Update the command in add_test to use $<TARGET_FILE:${AAST_TEST_NAME}>
      instead of specifying the ELF file directly.
    
    * fix: CMAKE_MODULE_PATH setting in CMakeLists.txt
    
    - Moved the CMAKE_MODULE_PATH setting back to its post-project position
    - Ensures proper module path configuration for the project
    
    * fix: add missing recommendation for C++ tools in extensions.json
    
    Added "ms-vscode.cpptools-extension-pack" to the list of
    recommended extensions for improved C development support.

---
+++ cmake/arm-semihosting.cmake
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2025, Roy Ratcliffe, Northumberland, United Kingdom
+# CMake module for setting up ARM semihosting tests.
+
+# CMake function to add ARM semihosting tests.
+# Parameters:
+# TEST_NAME - Name of the test executable.
+# TEST_SOURCES - List of source files for the test.
+# APP_SOURCES - List of application source files to include in the test.
+# Usage:
+# add_arm_semihosting_test(TEST_NAME my_test
+#     TEST_SOURCES
+#         test1.c
+#         test2.c
+#     APP_SOURCES
+#         app1.c
+#         app2.c
+# )
+function(add_arm_semihosting_test)
+    set(options)
+    set(oneValueArgs TEST_NAME)
+    set(multiValueArgs TEST_SOURCES APP_SOURCES)
+    cmake_parse_arguments(AAST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    add_executable(${AAST_TEST_NAME})
+    target_sources(${AAST_TEST_NAME} PRIVATE
+        ${AAST_TEST_SOURCES}
+        ${AAST_APP_SOURCES}
+        ${CMAKE_SOURCE_DIR}/Core/Src/system_stm32f4xx.c
+        ${CMAKE_SOURCE_DIR}/startup_stm32f407xx.s
+    )
+    target_include_directories(${AAST_TEST_NAME} PRIVATE ${ARMSemihostingIncludeDirectories})
+    target_compile_definitions(${AAST_TEST_NAME} PRIVATE ${ARMSemihostingCompileDefinitions})
+
+    # Link against the ARM Cortex-M4 math library.
+    target_link_libraries(${AAST_TEST_NAME} PRIVATE arm_cortexM4lf_math)
+
+    target_link_options(${AAST_TEST_NAME} PRIVATE --specs=rdimon.specs -lrdimon)
+    set_target_properties(${AAST_TEST_NAME} PROPERTIES TIMEOUT 30)
+    add_test(NAME ${AAST_TEST_NAME} COMMAND ${CMAKE_CROSSCOMPILING_EMULATOR} $<TARGET_FILE:${AAST_TEST_NAME}>)
+endfunction()
