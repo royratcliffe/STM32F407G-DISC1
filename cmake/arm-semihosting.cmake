8d067f3cf9df278dbd08296c1414017f3c113736
Roy Ratcliffe <roy@ratcliffe.me>
2026-01-12 18:56:22 +0000

    Enhance ARM semihosting test function and documentation (#5)
    * feat: enhance add_arm_semihosting_test function
    
    Add support for SYSTEM_SOURCES and LINK_LIBRARIES parameters in
    add_arm_semihosting_test function. Update the function to include
    timeout handling and improve source file management for tests.
    
    * feat: add ARM semihosting system sources and link libraries
    
    Include system_stm32f4xx.c and startup_stm32f407xx.s in the
    ARMSemihostingSystemSources. Link the arm_cortexM4lf_math library
    to all tests.
    
    * feat: enhance ring buffer documentation
    
    Improve documentation for ring buffer structures and functions.
    Add detailed descriptions for ring buffer zones and item handling.
    Update return values in function documentation for consistency.
    
    * feat: update documentation for add_arm_semihosting_test function
    
    Add missing parameters for SYSTEM_SOURCES and LINK_LIBRARIES in the
    documentation of the add_arm_semihosting_test function. Update
    example usage to reflect correct library names.
    
    * docs: update documentation for add_arm_semihosting_test function
    
    Add description for optional TIMEOUT parameter in the usage section.

--- cmake/arm-semihosting.cmake
+++ cmake/arm-semihosting.cmake
@@ -7,6 +7,9 @@
 # TEST_NAME - Name of the test executable.
 # TEST_SOURCES - List of source files for the test.
 # APP_SOURCES - List of application source files to include in the test.
+# SYSTEM_SOURCES - List of system source files, e.g. startup code.
+# LINK_LIBRARIES - List of libraries to link against.
+# TIMEOUT - Optional timeout for the test.
 # Usage:
 # add_arm_semihosting_test(TEST_NAME my_test
 #     TEST_SOURCES
@@ -15,27 +18,45 @@
 #     APP_SOURCES
 #         app1.c
 #         app2.c
+#     SYSTEM_SOURCES
+#         system1.c
+#         system2.c
+#     LINK_LIBRARIES
+#         library1
+#         library2
 # )
 function(add_arm_semihosting_test)
     set(options)
-    set(oneValueArgs TEST_NAME)
-    set(multiValueArgs TEST_SOURCES APP_SOURCES)
+    set(oneValueArgs TEST_NAME TIMEOUT)
+    set(multiValueArgs TEST_SOURCES APP_SOURCES SYSTEM_SOURCES LINK_LIBRARIES)
     cmake_parse_arguments(AAST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     add_executable(${AAST_TEST_NAME})
     target_sources(${AAST_TEST_NAME} PRIVATE
+        ${ARMSemihostingTestSources}
         ${AAST_TEST_SOURCES}
+        ${ARMSemihostingAppSources}
         ${AAST_APP_SOURCES}
-        ${CMAKE_SOURCE_DIR}/Core/Src/system_stm32f4xx.c
-        ${CMAKE_SOURCE_DIR}/startup_stm32f407xx.s
+        ${ARMSemihostingSystemSources}
+        ${AAST_SYSTEM_SOURCES}
     )
     target_include_directories(${AAST_TEST_NAME} PRIVATE ${ARMSemihostingIncludeDirectories})
     target_compile_definitions(${AAST_TEST_NAME} PRIVATE ${ARMSemihostingCompileDefinitions})
 
-    # Link against the ARM Cortex-M4 math library.
-    target_link_libraries(${AAST_TEST_NAME} PRIVATE arm_cortexM4lf_math)
+    # Link against the any additional libraries, e.g. ARM Cortex-M4 math.
+    target_link_libraries(${AAST_TEST_NAME} PRIVATE
+        ${ARMSemihostingLinkLibraries}
+        ${AAST_LINK_LIBRARIES}
+    )
 
+    # Link with semihosting specifications. RDI monitor provides semihosting
+    # support for ARM targets.
     target_link_options(${AAST_TEST_NAME} PRIVATE --specs=rdimon.specs -lrdimon)
-    set_target_properties(${AAST_TEST_NAME} PROPERTIES TIMEOUT 30)
+
+    # Set timeout for the test if specified.
+    if(AAST_TIMEOUT)
+        set_target_properties(${AAST_TEST_NAME} PROPERTIES TIMEOUT ${AAST_TIMEOUT})
+    endif()
+
     add_test(NAME ${AAST_TEST_NAME} COMMAND ${CMAKE_CROSSCOMPILING_EMULATOR} $<TARGET_FILE:${AAST_TEST_NAME}>)
 endfunction()
